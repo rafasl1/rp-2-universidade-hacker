2942336030a1a803670b0c42cc5d0a55c6e32cc2
agspf94 <anderson.fantin@usp.br>
2021-10-11 15:04:41 -0300

    Inicio do backend

---
+++ src/javascript/script.js
@@ -0,0 +1,57 @@
+/*
+
+Criação do objeto perguntas
+
+Leitura do json de perguntas em lista
+Selecionar X (x = 5) perguntas aleatoriamente da lista e definir respostas corretas
+Mostrar pergunta x na tela
+Determinar se a resposta escolhida está correta e salvar essa informação
+Na ultima pagina, ao clicar me finalizar,
+	calcular pontuação e
+	mostrar resultado na tela
+
+questionsList = [] // TODAS AS PERGUNTAS QUE EXISTEM NO JSON
+questionsUsedList = [pergunta1, pergunta2, pergunta3, pergunta4, pergunta5] // PERGUNTAS Q VAO SER USADAS
+correctAnswers = [2,3,0,1,1] // VETOR DE RESPOSTAS
+userAnswers = [0,2,3,2,1] // RESPOSTAS DO USUÁRIO
+
+*/
+
+const question = {
+
+}
+
+questionsList = [] // TODAS AS PERGUNTAS QUE EXISTEM NO JSON
+questionsUserList = [pergunta1, pergunta2, pergunta3, pergunta4, pergunta5] // PERGUNTAS Q VAO SER USADAS
+correctAnswers = [2,3,0,1,1] // VETOR DE RESPOSTAS
+userAnswers = [0,2,3,2,1] // RESPOSTAS DO USUÁRIO
+perguntaX = 0
+
+function startQuiz() { // fácil afonso
+
+}
+
+function readJson() { // difícil afonso
+    // set questionsList
+}
+
+function selectRandom(x = 5) { // fácil afonso
+    // set questionsUserList
+}
+
+function showQuestion(question = 1) { // média afonso
+
+}
+
+function submitAnswer() { // médio and
+    // update userAnswers
+    // show nextQuestion
+}
+
+function showResult() { // médio and
+
+}
+
+function calculatePoints() { // fácil and
+
+}
