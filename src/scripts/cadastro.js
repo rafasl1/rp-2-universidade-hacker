ca41791f8750ab1d60d3d3ed4a08e0f48cb23548
Rafael <rafael.sl@usp.br>
2021-11-21 21:19:11 -0300

    Adicioona método de login dinamicamente

---
+++ src/scripts/cadastro.js
@@ -0,0 +1,23 @@
+function cadastrarPlayer() {
+  fetch("http://localhost:8080/cadastro", {
+    method: "POST",
+    body: JSON.stringify({
+      name: document.getElementById('real-name').value,
+      username: document.getElementById('username').value,
+      password: document.getElementById('password').value
+    }),
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(response => response.json())
+    .then(response => {
+      console.log(response);
+      localStorage.setItem("Player Logado", JSON.stringify(response));
+    })
+    .catch(error => {
+      throw new Error(`Error on sign up: ${error}`);
+    })
+  document.location.href = "index.html";
+}
