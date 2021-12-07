567a640a2940fdc4cc1d1e0d915820d6d506819c
Rafael <rafael.sl@usp.br>
2021-12-06 23:59:58 -0300

    Arruma bug da dificuldade

--- src/scripts/selecionarDificuldade.js
+++ src/scripts/selecionarDificuldade.js
@@ -11,9 +11,6 @@ function startQuiz() {
     case 'Primo que conserta o Wi-Fi':
       dificuldade = "primo-que-conserta-o-wifi"
       break;
-    case 'Hackeia a NASA':
-      dificuldade = "hackeia-a-nasa"
-      break;
     default:
       throw new Error("Invalid option");
   }
@@ -37,30 +34,22 @@ function startQuiz() {
 }
 
 function selectOption(option) {
+  console.log(option)
   switch (option) {
     case 'firstOption':
       document.getElementById(option).style.border = "8px solid #0FBFFF";
       document.getElementById('SecondOption').style.border = "";
       document.getElementById('thirdOption').style.border = "";
-      document.getElementById('fourthOption').style.border = "";
       break;
     case 'SecondOption':
       document.getElementById(option).style.border = "8px solid #FF960F";
       document.getElementById('firstOption').style.border = "";
       document.getElementById('thirdOption').style.border = "";
-      document.getElementById('fourthOption').style.border = "";
       break;
     case 'thirdOption':
       document.getElementById(option).style.border = "8px solid #1E91E8";
       document.getElementById('firstOption').style.border = "";
       document.getElementById('SecondOption').style.border = "";
-      document.getElementById('fourthOption').style.border = "";
-      break;
-    case 'fourthOption':
-      document.getElementById(option).style.border = "8px solid #3915B0";
-      document.getElementById('firstOption').style.border = "";
-      document.getElementById('SecondOption').style.border = "";
-      document.getElementById('thirdOption').style.border = "";
       break;
     default:
       throw new Error("Invalid option");
